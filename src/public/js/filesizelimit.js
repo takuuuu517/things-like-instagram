103b315fff9ec13db2f1d23be9c911432a6d9cd6
takuuuu517 <ryoryoryoasa@gmail.com>
2019-05-18 14:52:30 +0900

    in progress

--- src/public/js/filesizelimit.js
+++ src/public/js/filesizelimit.js
@@ -1,10 +1,12 @@
 function fileupload() {
-    console.log('helo');
 
 
     var uploadField = document.getElementById("file");
+    console.log(uploadField);
 
     uploadField.onchange = function () {
+        console.log('helo');
+
         if (this.files[0].size > 307200) {
             alert("File is too big!");
             this.value = "";
