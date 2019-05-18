02218493bdb645cc0515f927aae815154f0e8773
takuuuu517 <ryoryoryoasa@gmail.com>
2019-05-18 16:33:49 +0900

    in progress

--- src/public/js/filesizelimit.js
+++ src/public/js/filesizelimit.js
@@ -1,18 +1,18 @@
-    var uploadField = document.getElementById("file");
+var uploadField = document.getElementById("file");
+var path = uploadField.value;
 
-    var extensions = /(\.jpg|\.png|\.gif)$/i;
+var extensions = /(\.jpg|\.png|\.gif)$/i;
 
 
-    uploadField.onchange = function () {
-        if (this.files[0].size > 60000000) {
-            alert("ファイルのサイズの上限は60MBです。");
-            this.value = "";
-        };
-
-        if(!extensions.exec(uploadField)){
-            alert('jpg png gif が有効です。');
-            fileInput.value = '';
-            return false;
-        };
+uploadField.onchange = function () {
+    if (this.files[0].size > 60000000) {
+        alert("ファイルのサイズの上限は60MBです。");
+        this.value = "";
     };
+
+    if(!extensions.exec(path)){
+        alert('jpg png gif が有効です。');
+        this.value = '';
+    };
+};
 
