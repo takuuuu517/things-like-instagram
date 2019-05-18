ba7e24cc88c2c7eba51f99a67c62a0e348a6dc2c
takuuuu517 <ryoryoryoasa@gmail.com>
2019-05-18 14:32:27 +0900

    in progress

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -31,57 +31,59 @@ class PostController extends Controller
 
     public function upload(Request $request)
     {
-        $token = $request->session()->get('github_token', null);
-        $github_user = Socialite::driver('github')->userFromToken($token);
-        $user = User::where('github_id',$github_user->user['login'] )->first();
 
-
-        $this->validate($request, [
-            'file' => [
-                // 必須
-                'required',
-                // アップロードされたファイルであること
-                'file',
-                // 画像ファイルであること
-                'image',
-                // MIMEタイプを指定
-                'mimes:jpeg,png,gif',
-                'file' => 'max:60000',
-            ],
-            'caption' => [
-                'max:200',
-            ]
-        ]);
-
-        if ($request->file('file')->isValid([]) ) {
-            $image = base64_encode(file_get_contents($request->file->getRealPath()));
-
-
-            Post::insert([
-                "image" => $image,
-                "user_id" => $user->id,
-                "picture_path" => 'hello',
-                "caption" => $request->input('caption'),
-            ]);
-            return redirect('/home');
-
-//            $path = $request->file->store('public');
-//            $post = new Post;
-//            $post->user_id = $user->id;
-////            $post->picture_path = basename($path);
-//            $post->caption = $request->input('caption');
-////            $image = base64_encode(file_get_contents($request->file->getRealPath()));
-////            $post->image = $image;
+        return dd("hello");
+//        $token = $request->session()->get('github_token', null);
+//        $github_user = Socialite::driver('github')->userFromToken($token);
+//        $user = User::where('github_id',$github_user->user['login'] )->first();
 //
-//            $post->picture_path = 'asdf';
-//            $post->image = 'hello';
-//            $post->save();
-
-        } else {
-            return redirect()
-                ->back()
-                ->withInput()
-                ->withErrors();
-        }
+//
+//        $this->validate($request, [
+//            'file' => [
+//                // 必須
+//                'required',
+//                // アップロードされたファイルであること
+//                'file',
+//                // 画像ファイルであること
+//                'image',
+//                // MIMEタイプを指定
+//                'mimes:jpeg,png,gif',
+//                'file' => 'max:60000',
+//            ],
+//            'caption' => [
+//                'max:200',
+//            ]
+//        ]);
+//
+//        if ($request->file('file')->isValid([]) ) {
+//            $image = base64_encode(file_get_contents($request->file->getRealPath()));
+//
+//
+//            Post::insert([
+//                "image" => $image,
+//                "user_id" => $user->id,
+//                "picture_path" => 'hello',
+//                "caption" => $request->input('caption'),
+//            ]);
+//            return redirect('/home');
+//
+////            $path = $request->file->store('public');
+////            $post = new Post;
+////            $post->user_id = $user->id;
+//////            $post->picture_path = basename($path);
+////            $post->caption = $request->input('caption');
+//////            $image = base64_encode(file_get_contents($request->file->getRealPath()));
+//////            $post->image = $image;
+////
+////            $post->picture_path = 'asdf';
+////            $post->image = 'hello';
+////            $post->save();
+//
+//        } else {
+//            return redirect()
+//                ->back()
+//                ->withInput()
+//                ->withErrors();
+//        }
     }
 }
