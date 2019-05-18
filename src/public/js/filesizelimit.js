e69c99ed4ec417420f9441186e70ae71264ac46f
takuuuu517 <ryoryoryoasa@gmail.com>
2019-05-18 16:37:15 +0900

    in progress

--- src/public/js/filesizelimit.js
+++ src/public/js/filesizelimit.js
@@ -1,18 +1,9 @@
 var uploadField = document.getElementById("file");
-var path = uploadField.value;
-
-var extensions = /(\.jpg|\.png|\.gif)$/i;
-
 
 uploadField.onchange = function () {
     if (this.files[0].size > 60000000) {
         alert("ファイルのサイズの上限は60MBです。");
         this.value = "";
-    };
-
-    if(!extensions.exec(path)){
-        alert('jpg png gif が有効です。');
-        this.value = '';
     };
 };
 
